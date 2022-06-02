46b9978f25cc342870eb3cd03df7ae93a4103145
Pedro <pedrolucaspalma@gmail.com>
2022-06-02 14:00:29 -0300

    fill new script

--- scripts/aws-sam-cli.sh
+++ scripts/aws-sam-cli.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+# Verify the integrity and authenticity of the downloaded installer files by generating a hash value using the following command:
+sha256sum aws-sam-cli-linux-x86_64.zip
+
+# The output should look like the following example:
+#  <64-character SHA256 hash value> aws-sam-cli-linux-x86_64.zip
+
+# Unzip the installation files into the sam-installation/ subdirectory.
+unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+
+# Install the AWS SAM CLI.
+sudo ./sam-installation/install
+
+# Verify the installation.
+sam --version
