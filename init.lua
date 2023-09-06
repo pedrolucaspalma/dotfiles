6e8e7ace775743acdebd96e31161ba4993da2a1d
Pedro Palma <pedrolucaspalma@gmail.com>
2023-09-06 07:42:48 -0300

    upd init.lua

--- init.lua
+++ init.lua
@@ -243,6 +243,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
   defaults = {
+    file_ignore_patterns = {
+      "node_modules","logs","build"
+    },
     mappings = {
       i = {
         ['<C-u>'] = false,
