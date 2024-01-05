7188fb058ed777d5fd0269e111910d0d9249b4e0
Pedro Palma <pedrolucaspalma@gmail.com>
2024-01-05 07:38:19 -0300

    rename vim config files

--- nvim/lua/lsp-setup.lua
+++ nvim/lua/lsp-setup.lua
@@ -52,9 +52,9 @@ end
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  tsserver                        = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs' } },
   -- emmet_language_server = {},
+  tsserver                        = {},
   eslint                          = {},
   gopls                           = {},
   emmet_ls                        = {},
