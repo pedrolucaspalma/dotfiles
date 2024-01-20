3fe71eed66e63b66d376ada6a56d31cbac1c3b49
Pedro Palma <pedrolucaspalma@gmail.com>
2024-01-20 07:52:33 -0300

    Add commented harpoon with bindings

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -23,7 +23,10 @@ require('lazy').setup({
 
   -- Color preview
   'norcalli/nvim-colorizer.lua',
-  --
+
+  -- Harpoon
+  -- 'ThePrimeagen/harpoon',
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
