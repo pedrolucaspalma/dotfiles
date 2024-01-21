a273c38878e0f2f4e9cd64bc34e6b598b8526944
Pedro Palma <pedrolucaspalma@gmail.com>
2024-01-21 17:47:55 -0300

    Comment friendly-snippets

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -75,7 +75,7 @@ require('lazy').setup({
       'hrsh7th/cmp-nvim-lsp',
 
       -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
+      -- 'rafamadriz/friendly-snippets',
     },
   },
 
