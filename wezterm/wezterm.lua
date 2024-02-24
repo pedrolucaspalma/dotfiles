9028c070e55157f78e394fbaa5895b24475ccb15
Pedro Palma <pedrolucaspalma@gmail.com>
2024-02-24 17:25:31 -0300

    upd wezterm fonts and theme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,9 +7,13 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
+--
 -- config.color_scheme = 'AdventureTime'
-config.color_scheme = 'Catppuccin Mocha'
--- config.font = wezterm.font 'Jet
+-- config.color_scheme = 'Batman'
+-- config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = 'Dracula'
+
+config.font = wezterm.font 'JetBrains Mono'
 
 -- and finally, return the configuration to wezterm
 return config
