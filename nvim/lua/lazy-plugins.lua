04dc485bd1b324ee2b9f6f3a15dac73610fff6e3
Pedro Palma <pedrolucaspalma@gmail.com>
2024-03-17 09:01:06 -0300

    add copilot

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -27,6 +27,9 @@ require('lazy').setup({
   -- Harpoon
   'ThePrimeagen/harpoon',
 
+  -- Copilot
+  'github/copilot.vim',
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
