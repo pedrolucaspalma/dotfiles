79bb5917e9168cfd98e9cacf4e6390a331c38701
Pedro Palma <pedrolucaspalma@gmail.com>
2024-08-26 13:42:38 -0300

    upd

--- install-dot.sh
+++ install-dot.sh
@@ -2,5 +2,15 @@
 
 rm -rf ~/.config/nvim
 
-ln -sf ~/Developer/dotfiles/nvim ~/.config/nvim
+# .zshrc
 ln -sf ~/Developer/dotfiles/.zshrc ~/.zshrc
+
+# tmux
+ln -sf ~/Developer/dotfiles/tmux.conf ~/tmux.conf 
+
+# wifi -> olhar wifi.md
+
+# neovim configs
+ln -sf ~/Developer/dotfiles/ ~/.config/nvim
+ln -sf ~/Developer/dotfiles/nvim ~/.config/nvim
+
