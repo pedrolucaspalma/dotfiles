5a02386444555994fca44618286cee158c745c2b
Pedro Palma <pedrolucaspalma@gmail.com>
2025-03-21 09:31:59 -0300

    Add barbar again

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -61,23 +61,23 @@ require('lazy').setup({
     'stevearc/conform.nvim',
     opts = {},
   },
-  -- {
-  --   'romgrk/barbar.nvim',
-  --   dependencies = {
-  --     'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
-  --     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-  --   },
-  --   opts = {
-  --     -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-  --     -- animation = true,
-  --     -- insert_at_start = true,
-  --     -- …etc.
-  --   },
-  --   init = function()
-  --     vim.g.barbar_auto_setup = false
-  --   end,
-  --   version = '^1.0.0', -- optional: only update when a new 1.x version is released
-  -- },
+  {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      -- animation = true,
+      -- insert_at_start = true,
+      -- …etc.
+    },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  },
   {
     -- Autocompletion
     'hrsh7th/nvim-cmp',
