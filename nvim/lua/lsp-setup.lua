d4dab9fb1dfa4f4641a157f62ec59b81f9a1a70e
Pedro Palma <pedrolucaspalma@gmail.com>
2025-03-25 10:26:52 -0300

    Remove goimports from mason file configuration

--- nvim/lua/lsp-setup.lua
+++ nvim/lua/lsp-setup.lua
@@ -54,7 +54,6 @@ end
 local servers = {
   eslint                          = {},
   gopls                           = {},
-  goimports                       = {},
   emmet_ls                        = {},
   dockerls                        = {},
   docker_compose_language_service = {},
