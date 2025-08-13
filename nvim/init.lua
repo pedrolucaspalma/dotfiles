47fd5285da9fb2268e47b498503bfa402f7ceeb1
Pedro Lucas Barros Palma <pedrolucaspalma@gmail.com>
2025-08-13 15:21:38 -0300

    remove colorizer

--- nvim/init.lua
+++ nvim/init.lua
@@ -25,6 +25,5 @@ require 'cmp-setup'
 require 'conform-setup'
 require 'theme'
 require 'barbar-setup'
-require 'colorizer-setup'
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
