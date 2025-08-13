37f3773f37b346fe1cef02830d1e03b0bc7c677e
Pedro Lucas Barros Palma <pedrolucaspalma@gmail.com>
2025-08-13 19:07:40 -0300

    move stuff to init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,20 +1,73 @@
+-- [[ Setting options ]]
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.o.hlsearch = false
+vim.wo.number = true
+vim.o.mouse = 'a'
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.wo.signcolumn = 'yes'
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.completeopt = 'menuone,noselect'
+vim.o.termguicolors = true
+vim.o.relativenumber = true
+vim.opt.swapfile = false
+vim.o.clipboard = 'unnamedplus'
+vim.opt.wrap = false
 -- vim.g.loaded_netrw = 1
 -- vim.g.loaded_netrwPlugin = 1
 
-vim.opt.termguicolors = true
+-- [[ Keymaps options ]]
+-- Fast Indenting
+vim.keymap.set("n", "<Tab>", ">>")
+vim.keymap.set("n", "<S-Tab>", "<<")
+vim.keymap.set("v", "<Tab>", ">gv")
+vim.keymap.set("v", "<S-Tab>", "<gv")
 
-require 'lazy-bootstrap'
+-- Rebinding vertical movements to put the cursor at the middle of the screen
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "<F2>", vim.lsp.buf.rename)
+
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+vim.keymap.set('v', '<C-c>', '"+y', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-c>', '"+yy', { noremap = true, silent = true })
+
+-- [[ Package Manager ]]
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- [[ Lazy Plugins ]]
+
 require 'lazy-plugins'
 require 'lsp-setup'
--- require 'lsp-status'
 
 require 'statusline-setup'
 
-require 'options'
-require 'keymaps'
+
 
 require 'telescope-setup'
 require 'treesitter-setup'
