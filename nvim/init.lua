453229be9d0a93bcb20ef9f90f93c1b01d1d21a7
Pedro Lucas Barros Palma <pedrolucaspalma@gmail.com>
2025-08-13 16:57:25 -0300

    make lualine status line display the relative file path

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,20 +1,17 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- disable netrw at the very start of your init.lua
 -- vim.g.loaded_netrw = 1
 -- vim.g.loaded_netrwPlugin = 1
 
--- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
 require 'lazy-bootstrap'
 require 'lazy-plugins'
 require 'lsp-setup'
 -- require 'lsp-status'
+
+require 'statusline-setup'
 
 require 'options'
 require 'keymaps'
@@ -25,5 +22,4 @@ require 'cmp-setup'
 require 'conform-setup'
 require 'theme'
 require 'barbar-setup'
--- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
