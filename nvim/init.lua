c025e5197cba3b76463ad85857bb2ff7167681d1
Pedro Lucas Barros Palma <pedrolucaspalma@gmail.com>
2025-08-13 19:19:33 -0300

    move stuff to init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,9 +15,9 @@ vim.o.timeoutlen = 300
 vim.o.completeopt = 'menuone,noselect'
 vim.o.termguicolors = true
 vim.o.relativenumber = true
-vim.opt.swapfile = false
+vim.o.swapfile = false
 vim.o.clipboard = 'unnamedplus'
-vim.opt.wrap = false
+vim.o.wrap = false
 -- vim.g.loaded_netrw = 1
 -- vim.g.loaded_netrwPlugin = 1
 
@@ -61,18 +61,77 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- [[ Lazy Plugins ]]
-
 require 'lazy-plugins'
+
+-- [[ Theme ]]
+vim.cmd("colorscheme tokyonight")
+
+-- [[ LSP / Mason setup ]]
 require 'lsp-setup'
 
-require 'statusline-setup'
-
-
+-- [[ Status Line / Lualine setup ]]
+require('lualine').setup {
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        path = 1
+      }
+    },
+  }
+}
 
 require 'telescope-setup'
 require 'treesitter-setup'
 require 'cmp-setup'
-require 'conform-setup'
-require 'theme'
-require 'barbar-setup'
+
+-- [[ Setup Autocomplete / conform ]]
+require("conform").setup({
+  formatters_by_ft = {
+    lua = { "stylua" },
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescriptreact = { "prettier" },
+    tex = { "latexindent" },
+    go = { "goimports", "gofmt" },
+    html = { "djlint" },
+  },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+})
+
+
+-- [[ Setup Tabs / barbar ]]
+require('barbar').setup {
+  sidebar_filetypes = {
+    NvimTree = true,
+    undotree = { text = 'undotree' },
+  },
+  animation = true,
+  auto_hide = 1,
+  tabpages = false,
+  clickable = true,
+}
+
+-- Goto buffer in position...
+vim.keymap.set('n', '<M-1>', '<Cmd>BufferGoto 1<CR>')
+vim.keymap.set('n', '<M-2>', '<Cmd>BufferGoto 2<CR>')
+vim.keymap.set('n', '<M-3>', '<Cmd>BufferGoto 3<CR>')
+vim.keymap.set('n', '<M-4>', '<Cmd>BufferGoto 4<CR>')
+-- vim.keymap.set('n', '<M-5>', '<Cmd>BufferGoto 5<CR>')
+-- vim.keymap.set('n', '<M-6>', '<Cmd>BufferGoto 6<CR>')
+-- vim.keymap.set('n', '<M-7>', '<Cmd>BufferGoto 7<CR>')
+-- vim.keymap.set('n', '<M-8>', '<Cmd>BufferGoto 8<CR>')
+-- vim.keymap.set('n', '<M-9>', '<Cmd>BufferGoto 9<CR>')
+
+vim.keymap.set('n', '<M-h>', '<Cmd>BufferPrevious<CR>')
+vim.keymap.set('n', '<M-l>', '<Cmd>BufferNext<CR>')
+vim.keymap.set('n', '<M-w>', '<Cmd>BufferClose<CR>')
+
+
+vim.g.barbar_auto_setup = false
 -- vim: ts=2 sts=2 sw=2 et
