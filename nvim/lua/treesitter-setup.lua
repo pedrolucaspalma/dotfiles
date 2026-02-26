9027c0ecb129eff0a699fd6bd72b863f14c36a48
Pedro Palma <pedrolucaspalma@gmail.com>
2026-02-26 08:28:15 -0300

    add syntax highlighting for js and css sessions in html files

--- nvim/lua/treesitter-setup.lua
+++ nvim/lua/treesitter-setup.lua
@@ -3,10 +3,10 @@
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
   ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim',
-    'elixir', 'templ', 'markdown' },
+    'elixir', 'templ', 'markdown', 'html', 'css' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-  auto_install = false,
+  auto_install = true,
 
   refactor = {
     -- smart_rename = {
